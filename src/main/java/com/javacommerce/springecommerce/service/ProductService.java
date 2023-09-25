622c9266c991ead305bb6e496da14d95063a5231
KonstantinDichev <kdichev.nl@gmail.com>
2023-09-25 22:20:01 +0200

    product service implementation

---
+++ src/main/java/com/javacommerce/springecommerce/service/ProductService.java
@@ -0,0 +1,46 @@
+package com.javacommerce.springecommerce.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.javacommerce.springecommerce.dto.ProductRequest;
+import com.javacommerce.springecommerce.dto.ProductResponse;
+import com.javacommerce.springecommerce.model.Product;
+import com.javacommerce.springecommerce.repository.ProductRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+     private final ProductRepository productRepository;
+
+    public void createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+
+        productRepository.save(product);
+        log.info("Product {} is saved", product.getId());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
+}
