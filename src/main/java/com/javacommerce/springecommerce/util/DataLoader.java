c3bfa299a496872cf9f533434ce5afe9674fdbb4
KonstantinDichev <kdichev.nl@gmail.com>
2023-09-29 22:10:06 +0200

    product utils

---
+++ src/main/java/com/javacommerce/springecommerce/util/DataLoader.java
@@ -0,0 +1,28 @@
+package com.javacommerce.springecommerce.util;
+
+import com.javacommerce.springecommerce.model.Product;
+import com.javacommerce.springecommerce.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+@RequiredArgsConstructor
+public class DataLoader implements CommandLineRunner {
+
+    private final ProductRepository productRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (productRepository.count() < 1) {
+            Product product = new Product();
+            product.setName("iPhone 13");
+            product.setDescription("iPhone 13");
+            product.setPrice(BigDecimal.valueOf(1000));
+
+            productRepository.save(product);
+        }
+    }
+}
