998e994d9e44e8c55c9ca2acfe6192c88a6c7dbf
Konstantin Dichev <kdichev.nl@gmail.com>
2023-10-06 21:58:58 +0200

    Create Order.java

---
+++ order-service/src/main/java/orderservice/model/Order.java
@@ -0,0 +1,24 @@
+package orderservice.model
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "orders_table")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String orderNumber;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<OrderItems> orderItemsList;
+}
