63b60b16687bccbda9b05d056b5ce781c41cf6cc
KonstantinDichev <kdichev.nl@gmail.com>
2023-10-18 21:18:14 +0200

    Add inventory repository and application.properties to inventory service

--- inventory-service/src/main/java/com/javacommerce/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/javacommerce/inventoryservice/repository/InventoryRepository.java
@@ -1,3 +1,6 @@
-public interface InventoryRepository {
-    
+package com.javacommerce.inventoryservice.repository;
+import com.javacommerce.inventoryservice.model.InventoryModel;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface InventoryRepository extends MongoRepository<InventoryModel, String> {
 }
