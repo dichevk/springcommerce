c553408c58441d45b7524384bb018ef0aacd69b9
KonstantinDichev <kdichev.nl@gmail.com>
2023-10-18 21:22:33 +0200

    Add inventory model

--- inventory-service/src/main/java/com/javacommerce/inventoryservice/model/InventoryModel.java
+++ inventory-service/src/main/java/com/javacommerce/inventoryservice/model/InventoryModel.java
@@ -1,3 +1,26 @@
+package com.javacommerce.inventoryservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.math.BigDecimal;
+import lombok.Getter;
+import lombok.Setter;
+
+@Document(value = "inventory")
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Data
+@Builder
 public class InventoryModel {
-    
+    @Id
+    private String id; 
+    private Integer quantity;
+    private String code; 
 }
