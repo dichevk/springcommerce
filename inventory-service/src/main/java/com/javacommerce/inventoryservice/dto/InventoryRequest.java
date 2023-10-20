1769958d05676eff8bc7d7d8b3fa7fc429bf7948
KonstantinDichev <kdichev.nl@gmail.com>
2023-10-20 23:59:29 +0200

    service implementation

--- inventory-service/src/main/java/com/javacommerce/inventoryservice/dto/InventoryRequest.java
+++ inventory-service/src/main/java/com/javacommerce/inventoryservice/dto/InventoryRequest.java
@@ -1,3 +1,15 @@
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 public class InventoryRequest {
-    
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class InventoryResponse {
+    private String skuCode;
+    private boolean isInStock;
+}
 }
--- inventory-service/src/main/java/com/javacommerce/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/javacommerce/inventoryservice/service/InventoryService.java
@@ -1,6 +1,25 @@
 package com.javacommerce.inventoryservice.service;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import com.javacommerce.inventoryservice.dto.InventoryRequest;
 import com.javacommerce.inventoryservice.repository.InventoryRepository;
 public class InventoryService {
     private final InventoryRepository inventoryRepository;
+    @Transactional(readOnly = true)
+    @SneakyThrows
+    public List<InventoryRequest> isInStock(List<String> skuCode) {
+        log.info("Checking Inventory");
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+                .map(inventory ->
+                        InventoryRequest.builder()
+                                .skuCode(inventory.getSkuCode())
+                                .isInStock(inventory.getQuantity() > 0)
+                                .build()
+                ).toList();
+    }
 }
