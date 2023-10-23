6db2d51680039ec9f612fa971bc2ae7debc9dc11
KonstantinDichev <kdichev.nl@gmail.com>
2023-10-23 22:17:09 +0200

    inventory controller implementation

--- inventory-service/src/main/java/com/javacommerce/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/javacommerce/inventoryservice/controller/InventoryController.java
@@ -1,3 +1,25 @@
+package com.javacommerce.inventoryservice.controller;
+
+import com.javacommerce.inventoryservice.dto.InventoryResponse;
+import com.javacommerce.inventoryservice.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/inventory")
+@RequiredArgsConstructor
+@Slf4j
 public class InventoryController {
-    
+
+    private final InventoryService inventoryService;
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<InventoryResponse> isInStock(@RequestParam List<String> barCode) {
+        log.info("Received inventory check request for barCode: {}", barCode);
+        return inventoryService.isInStock(barCode);
+    }
 }
